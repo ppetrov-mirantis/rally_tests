4692ab450689ec7647d6ac2b814db60ca83d6885
Pavel Petrov <ppetrov@mirantis.com>
2016-09-19 19:20:55 +0300

    Infrastructure reconfig

--- keystone_s3api_via_rgw/prepare_node.sh
+++ keystone_s3api_via_rgw/prepare_node.sh
@@ -3,11 +3,17 @@ apt-get update && apt-get install --yes --force-yes python-pip subversion
 pip install rally==0.6.0 filechunkio
 
 # download tests
-mkdir ~/files_to_upload ~/downloaded_files ~/.rally
-cd ~/.rally
+## create directories
+mkdir -p ~/files_to_upload ~/downloaded_files ~/.rally/plugins
+## download rally plugins
+cd ~/.rally/plugins
 svn export https://github.com/ppetrov-mirantis/rally_tests/trunk/keystone_s3api_via_rgw/plugins_s3
 svn export https://github.com/ppetrov-mirantis/rally_tests/trunk/keystone_s3api_via_rgw/tests
+## download test data
+svn export https://github.com/ppetrov-mirantis/rally_tests/trunk/keystone_s3api_via_rgw/test_data ~/
+tar xf ~/test_data/kern_source.tar -C ~/files_to_upload
 
+# configure rally
 scp node-3:~/openrc .
 . openrc admin admin
 rally-manage db recreate

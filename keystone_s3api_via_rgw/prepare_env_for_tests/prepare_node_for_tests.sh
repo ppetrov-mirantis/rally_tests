326c187cb49fb2403070e34d0d535cd6fadf41dd
Pavel Petrov <ppetrov@mirantis.com>
2016-09-19 13:38:06 +0300

    Infrastructure reconfig

--- keystone_s3api_via_rgw/prepare_env_for_tests/prepare_node_for_tests.sh
+++
@@ -1,8 +0,0 @@
-apt-get update && apt-get install --yes --force-yes python-pip subversion
-pip install rally==0.6.0 filechunkio
-
-mkdir ~/downloaded_files ~/files_to_upload ~/.rally
-
-cd ~/.rally
-svn export https://github.com/ppetrov-mirantis/rally_tests/trunk/keystone_s3api_via_rgw/plugins
-svn export https://github.com/ppetrov-mirantis/rally_tests/trunk/keystone_s3api_via_rgw/tests
--- keystone_s3api_via_rgw/prepare_env_for_tests/turn_on_s3_ks_auth_for_rgw.sh
+++
@@ -1,8 +0,0 @@
-
-ceph_computes=$(fuel nodes | grep controller | grep ceph | cut -f 5 -d "|" | tr -d " " | sort)
-
-for i in $ceph_computes; do
-  ssh root@$i "sed -i.bkp -- '/\[client.radosgw.gateway\]/a rgw_s3_auth_use_keystone = True' /etc/ceph/ceph.conf && /etc/init.d/radosgw restart"
-done
-
-
